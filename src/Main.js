b88dadd76312c42347bb6ba40bb3bae66a900788
Adrian Hintze <adrian.hintze@bazaarvoice.com>
2019-09-17 10:12:45 +0100

    initial server setup

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+
+const app = express(); // better instead
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+    response.sendfile('index.html', { root: path.join(__dirname, '..', 'site') } );
+});
+
+app.listen(8080);
